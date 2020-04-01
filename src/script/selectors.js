1f7994864c456c7f96d0340f9e922f3960807880
coderanac <coder.anac@gmail.com>
2020-04-01 18:29:11 -0300

    :sparkles: Create HTML element selectors

---
+++ src/script/selectors.js
@@ -0,0 +1,9 @@
+const broom = document.querySelector('#broom');
+const clear = document.querySelector('#clear');
+const decrypt =  document.querySelector('#decrypt');
+const encrypt = document.querySelector('#encrypt');
+const logo = document.querySelector('#logo');
+const offset = document.querySelector('#offset');
+const title = document.querySelector('#title');
+
+export { broom, clear, decrypt, encrypt, logo, offset, title };
